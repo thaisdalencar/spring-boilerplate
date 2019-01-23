64b931902b16efaf92651b33e7077a562bd985d7
thais alencar <thaisdalencar>
2019-01-22 21:43:34 -0300

    Adding log configutarion

--- src/main/java/com/thaisdalencar/book/BookApplication.java
+++ src/main/java/com/thaisdalencar/book/BookApplication.java
@@ -1,14 +1,19 @@
 package com.thaisdalencar.book;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger("Book.class");
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookApplication.class, args);
+        logger.info("Just check log config");
+    }
 
 }
 
